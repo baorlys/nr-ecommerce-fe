2694850f129a3eb5b4346ef82c22213a10dc3632
Ly Gia Bao <69019508+baorlys@users.noreply.github.com>
2025-04-24 16:58:31 +0700

    feat: Implement error boundary for improved error handling in the application

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { ErrorBoundary } from 'react-error-boundary'
+import ErrorFallback from './components/layout/ErrorFallback'
 import './index.css'
 import App from './App'
 
+function logErrorToConsole(error: Error, info: { componentStack?: string | null }) {
+  console.error('Error caught by ErrorBoundary:', error)
+  console.error('Component stack:', info.componentStack ?? 'No stack trace available')
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <ErrorBoundary FallbackComponent={ErrorFallback} onError={logErrorToConsole}>
+        <App />
+      </ErrorBoundary>
     </Provider>
   </StrictMode>,
 )
